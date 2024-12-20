06ba47a0ceb28b6d094db0a4cf92ddd365275c72
soufiiyane <soufiyane.aitmoulay@gmail.com>
2024-12-20 15:57:18 +0100

    cypress coverage working & project mvp & sonar unit test & coverage

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,24 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   fixturesFolder: false,
+  reporter: 'cypress-sonarqube-reporter',
+  reporterOptions: {
+    outputDir: 'coverage',
+    outputFile: 'cypress-report.xml',
+    overwrite: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // If you're using code coverage
+      require('@cypress/code-coverage/task')(on, config)
+      
+      // Your existing plugin setup
+      const pluginConfig = require('./cypress/plugins/index.js')(on, config)
+      
+      return {
+        ...pluginConfig,
+        // any additional configuration
+      }
     },
     baseUrl: 'http://localhost:3000',
   },
